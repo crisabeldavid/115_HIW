df89396114cb79d404504bc292831151078000fa
crisabeldavid <crisabel.david.iics@ust.edu.ph>
2018-11-08 00:46:06 +0800

    no more duplicate ids, added last screen

--- app/src/main/java/com/davidgella/hiw/ViewCardActivity.java
+++ app/src/main/java/com/davidgella/hiw/ViewCardActivity.java
@@ -5,12 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class UserActivity extends AppCompatActivity {
+public class ViewCardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.userpage);
+        setContentView(R.layout.viewcardpage);
     }
 
     public void toLogIn (View v) {
@@ -20,11 +20,6 @@ public class UserActivity extends AppCompatActivity {
 
     public void toProfile (View v) {
         Intent i = new Intent(this,ProfileActivity.class);
-        startActivity(i);
-    }
-
-    public void toViewCard (View v) {
-        Intent i = new Intent(this,ViewCardActivity.class);
         startActivity(i);
     }
 }
